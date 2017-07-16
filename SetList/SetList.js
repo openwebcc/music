73809d4764ff4bb992b5e73b4d656e66d7807205
Klaus Förster <openwebcc@github.com>
2017-07-16 13:40:06 +0200

    add SetList prototype for creating simple concert programs from LibreOffice sheet with HTML/CSS/Javascript

---
+++ SetList/SetList.js
@@ -0,0 +1,82 @@
+/* SetList JS library */
+
+window.onload = function () {
+    var elem = {
+        inputData : document.querySelector("#inputData"),
+        inputSubmit : document.querySelector("#inputSubmit"),
+        inputTitle : document.querySelector("#inputTitle"),
+        pageTitle : document.querySelector("#pageTitle"),
+        outTitle : document.querySelector("#outTitle"),
+        outProgram : document.querySelector("#outProgram"),
+        outMore : document.querySelector("#outMore"),
+    };
+    var createProgram = function () {
+        var rows, i, row, header, j, obj, optional, markup;
+        rows = elem.inputData.value.split("\n");
+        for (i = 0; i < rows.length; i += 1) {
+            row = rows[i].split("\t");
+            if (!header) {
+                header = row;
+            } else {
+                if (row.length != header.length) {
+                    // skip empty records
+                    continue;
+                }
+                // assign object
+                obj = {};
+                for (j = 0; j < header.length; j += 1) {
+                    obj[header[j].toLowerCase()] = row[j]
+                }
+
+                // format entry
+                // SORT	SONG	CLEF	INSTRUMENT	CHORDS	REMARK	SHEET	OPTIONAL
+
+                // prepare marking of optional entries
+
+                markup = [];
+                markup.push("<div class='entry " + optional + "'>");
+                markup.push("<p class='song'>" + obj.sort + ".&nbsp;" + obj.song);
+                if (obj.clef) {
+                    markup.push(" (" + obj.clef + ")");
+                }
+                if (obj.instrument) {
+                    markup.push(" <span class='instrument'>" + obj.instrument + "</span>");
+                }
+                if (obj.sheet) {
+                    markup.push(" <span class='sheet'>Sheet</span>");
+                }
+                markup.push("</p>");
+                if (obj.remark) {
+                    markup.push("<p class='remark'>" + obj.remark + "</p>");
+                }
+                if (obj.chords) {
+                    markup.push("<pre class='chords'>" + obj.chords + "</pre>");
+                }
+
+                markup.push("</p>");
+                markup.push("</div>");
+
+                // append to program
+                if (obj.optional) {
+                    elem.outMore.innerHTML += markup.join("");
+                } else {
+                    elem.outProgram.innerHTML += markup.join("");
+                }
+            }
+        }
+    };
+
+    // autoselect input title and data
+    elem.inputTitle.onfocus = elem.inputData.onfocus = function () {
+        this.select()
+    };
+    //
+    elem.inputSubmit.onclick = function () {
+        // set title and heading
+        elem.pageTitle.innerHTML = elem.inputTitle.value;
+        elem.outTitle.innerHTML = elem.inputTitle.value;
+
+        // populate program entries
+        createProgram();
+    };
+};
